d56fd6abec2a9344bc54021435abe50d574a096f
nadarihem <messoussa.rihem1479@yahoo.com>
2024-04-09 16:08:57 +0200

    chatbot

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/chatbot', methods=['POST'])
-def chatbot():
-    if request.method == 'POST':
-        data = request.get_json()
-        user_message = data['message']
-
-        # Code du ChatBot pour traiter user_message et générer une réponse
-        bot_response = "Bonjour, je suis un ChatBot !"
-
-        return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
     app.run(debug=True)
